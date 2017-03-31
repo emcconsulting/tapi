94253d41f0a61a0b1d0b9aac1ddfd67e7286a8e2
Amy <amy.tripathi@emc.com>
2017-03-31 12:18:19 +0530

    JavaDoc comments added to ExecutionServiceMgr class Signed-off-by: Amy <amy.tripathi@emc.com>

--- apitest-tool/execution/execution-service/src/main/java/com/codemetal/tapi/service/ExecutionServiceMgr.java
+++ apitest-tool/execution/execution-service/src/main/java/com/codemetal/tapi/service/ExecutionServiceMgr.java
@@ -13,18 +13,19 @@ import org.springframework.data.domain.Sort.Direction;
 import com.codemetal.tapi.repository.TestCaseRepository;
 import com.codemetal.tapi.repository.TestSuiteRepository;
 
-
-
 import com.codemetal.tapi.metadata.TestCaseDetails;
 import com.codemetal.tapi.metadata.TestSuite;
 
 /**
- * This class prvides services to execute the testsuites and testcases.
+ * This class provides services to execute the testsuites.
  * 
  * @author Amy Tripathi
  *
  */
 public class ExecutionServiceMgr {
+
+	public static final int TESTSUITE_PAGESIZE = 10;
+	public static final int TESTCASE_PAGESIZE = 50;
 
 	@Autowired
 	TestSuiteRepository testSuiteRepo;
@@ -32,14 +33,31 @@ public class ExecutionServiceMgr {
 	@Autowired
 	TestCaseRepository testCaseRepo;
 
+	/**
+	 * This method returns all the testsuites in paginated format using the
+	 * criteria provided in pageable
+	 * 
+	 * @param pageable
+	 *            This specifies the pagenumber, pagesize, sortkey and
+	 *            sortdirection to do the pagination of result
+	 * @return the collection containing all the testsuites
+	 */
 	public Collection<TestSuite> findAllTestSuites(Pageable pageable) {
 
-		Collection<TestSuite> results = null;
-		results = testSuiteRepo.findAll(pageable).getContent();
-		return results;
+		return testSuiteRepo.findAll(pageable).getContent();
 
 	}
 
+	/**
+	 * 
+	 * @param testSuiteId
+	 *            The Id of a testSuite for which all testcases are to be
+	 *            fetched
+	 * @param pageable
+	 *            This specifies the pagenumber, pagesize, sortkey and
+	 *            sortdirection to do the pagination of result
+	 * @return
+	 */
 	public Collection<TestCaseDetails> findTestCasesByTestSuiteId(String testSuiteId, Pageable pageable) {
 
 		Page<TestCaseDetails> results = testCaseRepo.findByTestSuiteId(testSuiteId, pageable);
@@ -49,32 +67,35 @@ public class ExecutionServiceMgr {
 
 	}
 
+	/**
+	 * This method executes all the testsuites present in the database
+	 */
 	public void executeTest() {
 
 		Collection<TestCaseDetails> testCaseDetails = null;
-		int size1 = 10;
-		int size = 50;
-		int page = 0;
-		int page1 = 0;
+		int testCasePageNumber = 0;
+		int testSuitePageNumber = 0;
 		Direction sortDir = Sort.Direction.ASC;
 		String sortKey = "name";
-		Long count1 = testSuiteRepo.count();
-		Long count = testCaseRepo.count();
-		for (long i = 0; i < count1 / size1; i++) {
-			Pageable pageable1 = new PageRequest(page1, size1, sortDir, sortKey);
+		Long testSuiteCount = testSuiteRepo.count();
+		Long testCaseCount = testCaseRepo.count();
+		for (long i = 0; i < testSuiteCount / ExecutionServiceMgr.TESTSUITE_PAGESIZE; i++) {
+			Pageable pageable1 = new PageRequest(testSuitePageNumber, ExecutionServiceMgr.TESTSUITE_PAGESIZE, sortDir,
+					sortKey);
 			for (TestSuite testSuite : findAllTestSuites(pageable1)) {
-				for (long j = 0; j < count / size; j++) {
-					Pageable pageable = new PageRequest(page, size, sortDir, sortKey);
+				for (long j = 0; j < testCaseCount / ExecutionServiceMgr.TESTCASE_PAGESIZE; j++) {
+					Pageable pageable = new PageRequest(testCasePageNumber, ExecutionServiceMgr.TESTCASE_PAGESIZE,
+							sortDir, sortKey);
 					testCaseDetails = findTestCasesByTestSuiteId(testSuite.getId(), pageable);
 					for (TestCaseDetails details : testCaseDetails) {
 						System.out.println(details.getScenarioDescription());
 
 					}
-					page = page + 1;
+					testCasePageNumber = testCasePageNumber + 1;
 				}
 
 			}
-			page1 = page1 + 1;
+			testSuitePageNumber = testSuitePageNumber + 1;
 
 		}
 

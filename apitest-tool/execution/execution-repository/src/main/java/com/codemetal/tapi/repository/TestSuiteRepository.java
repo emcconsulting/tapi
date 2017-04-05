482e6b911941a09a9bbf152d016d5f53dca33ce7
mohitpratapsingh <mohit.singh@emc.com>
2017-04-05 15:40:42 +0530

    Removing unused import

--- apitest-tool/execution/execution-repository/src/main/java/com/codemetal/tapi/repository/TestSuiteRepository.java
+++ apitest-tool/execution/execution-repository/src/main/java/com/codemetal/tapi/repository/TestSuiteRepository.java
@@ -3,8 +3,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.codemetal.tapi.metadata.TestSuite;
 
-import net.sf.ehcache.store.cachingtier.CountBasedBackEnd;
-
 public interface TestSuiteRepository extends PagingAndSortingRepository<TestSuite,Long> {
 	
 	
